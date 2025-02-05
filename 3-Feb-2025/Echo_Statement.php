d7bb772b7fb8660e44f79bf2ab8afc7143b0d92f
Ahsan Ali <93969884+ahsanaliSWE@users.noreply.github.com>
2025-02-06 00:51:27 +0500

    Add files via upload

---
+++ 3-Feb-2025/Echo_Statement.php
@@ -0,0 +1,105 @@
+<?php 
+                // Ahsan's CV
+
+echo "<html>";
+echo "<body>";
+
+echo "<div style='border:solid 2px' align='center'>";
+echo "<h1>Ahsan Ali</h1>";
+echo "<h3>Software Engineer || Flutter Developer || UI Designer || Deep Learning</h3>";
+echo "</div>";
+
+echo "<p align='center'>Date of Birth: 02/01/2003 | Nationality: Pakistani | Gender: Male | Phone: 03229057739</p>";
+
+echo "<hr>";
+echo "<div>";
+echo "<h2>Summary</h2>";
+echo "<p>A Software Engineer specializing in Flutter app development, Web development, Machine Learning, and Deep Learning. Experienced in MySQL/Firebase for backend integration, Node.js for API creation, and Postman for API testing. Skilled in Git/GitHub for version control and UI/UX design using Figma. Passionate about building scalable and efficient applications.</p>";
+echo "</div>";
+
+echo "<hr>";
+echo "<div>";
+echo "<h2>Work Experience</h2>";
+echo "<ul>";
+echo "<li><strong>Flutter Developer (May 2024 – Jul 2024)</strong></li>";
+echo "<p><strong>Freelancer (Fiverr)</strong></p>";
+echo "<p>Developed a Flutter application with Google Maps API for CRUD operations on Points of Interest (POIs).</p>";
+echo "<li><strong>Mobile Application Developer (Jan 2024 – Apr 2024)</strong></li>";
+echo "<p><strong>MUET Alumni Development Cell</strong></p>";
+echo "<p>Built a Flutter & Firebase mobile app for MUET, improving user engagement and Developed a Node.js API for backend services.</p>";
+echo "</ul>";
+echo "</div>";
+
+echo "<hr>";
+echo "<div>";
+echo "<h2>Education</h2>";
+echo "<ul>";
+echo "<li><strong>B.E Software Engineering (2020 – 2024)</strong></li>";
+echo "<p>Mehran University of Engineering and Technology (MUET), Jamshoro</p>";
+echo "<li><strong>Intermediate (2020)</strong></li>";
+echo "<p>The Educators College, Jamshoro</p>";
+echo "<li><strong>Matriculation (2018)</strong></li>";
+echo "<p>The Educators School, Jamshoro</p>";
+echo "</ul>";
+echo "</div>";
+
+
+echo "<hr>";
+echo "<div>";
+echo "<h2>Skills</h2>";
+echo "<ul>";
+echo "<li><strong>Programming & Development:</strong> Flutter, Dart, JavaScript, HTML, CSS, Bootstrap</li>";
+echo "<li><strong>Backend & Databases:</strong> Node.js, Express, MySQL, Firebase</li>";
+echo "<li><strong>Machine Learning & AI:</strong> TensorFlow, Deep Learning Models</li>";
+echo "<li><strong>Tools & Platforms:</strong> Git/GitHub, Postman, Google Cloud, AWS, Figma, Canva</li>";
+echo "</ul>";
+echo "</div>";
+
+
+echo "<hr>";
+echo "<div>";
+echo "<h2>Projects</h2>";
+echo "<ul>";
+echo "<li><strong>Dental Caries Detection App (2024)</strong>– Built with Flutter & TensorFlow for AI-based early dental disease detection.</li>";
+echo "<li><strong>CV Maker (2024):</strong>– A web app for generating professional CVs using HTML, CSS, JS, PHP & MySQL.</li>";
+echo "<li><strong>COVID-19 Tracker (2023)</strong>– A Flutter & Firebase app for real-time COVID case tracking.</li>";
+echo "</ul>";
+echo "</div>";
+
+
+echo "<hr>";
+echo "<div>";
+echo "<h2>Certificates</h2>";
+echo "<ul><strong>";
+echo "<li>Postman API Fundamentals Student Expert (May 2024)</li>";
+echo "<li>Foundations of Cybersecurity (Jul 2024)</li>";
+echo "<li>National Freelance Training Program on Technical Domain (2023)</li>";
+echo "<li>Python Fundamentals for Beginners (2022)</li>";
+echo "</strong></ul>";
+echo "</div>";
+
+echo "<hr>";
+echo "<div>";
+echo "<h2>Volunteering & Activities</h2>";
+echo "<ul>";
+echo "<li><strong>Rotaract Club of Hyderabad (2021 – Present)</strong>– Organized community service projects (blood donation, ration drives).</li>";
+echo "<li><strong>GDSC MUET (2020 – 2021)</strong>– Contributed to the Google Developer Student Club.</li>";
+echo "</ul>";
+echo "</div>";
+
+echo "<hr>";
+echo "<div>";
+echo "<h2>Hobbies & Interests</h2>";
+echo "<ul>";
+echo "<li>Video Gaming</li>";
+echo "<li>Tech Tinkering</li>";
+echo "<li>Traveling</li>";
+echo "<li>Video Gaming</li>";
+echo "<li>Video Gaming</li>";
+echo "</ul>";
+echo "</div>";
+echo "<hr>";
+
+echo "</body>";
+echo "</html>";
+?>
