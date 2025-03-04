5d3c78c9367978197d848687566639ca72e09712
Ahsan Ali <93969884+ahsanaliSWE@users.noreply.github.com>
2025-03-04 22:40:09 +0500

    Add files via upload

---
+++ 4-March-2025/login_session_2d/studentDashboard.php
@@ -0,0 +1,61 @@
+<?php
+    session_start();
+
+    if(isset($_SESSION['users'])){
+        if($_SESSION['users']['role'] == "teacher"){
+            header("location: teacherDashboard.php");
+        }elseif($_SESSION['users']['role'] == "admin"){
+            header("location: adminDashboard.php");
+        }
+    }else{
+        header("location: login_form.php?msg=Please Login First...");
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Student Dashboard</title>
+    <style>
+        body{
+            background-color: cyan;
+        }
+        h1{
+            color: black;
+            background-color:lightgray
+        }
+        a{
+            text-decoration: none;
+            color: black;
+            background-color: deepskyblue;
+            border: 1px solid black;
+        }
+        a:hover{
+            color:red;
+        }
+    </style>
+</head>
+<body>
+    <center>
+    <h1>Student Dashboard</h1>
+    <hr>
+    <h2>Welcome... <?php echo $_SESSION['users']["first_name"]." ".$_SESSION['users']["last_name"]; ?></h2>
+    <a href="logout.php">Logout</a>
+        <p>First Name : <?php echo $_SESSION['users']["first_name"]."<br/>"?><p>
+        <p>Last Name : <?php echo $_SESSION['users']["last_name"]."<br/>"?><p>
+        <p>Email : <?php echo $_SESSION['users']["email"]."<br/>"?><p>
+        <p>Password : <?php echo $_SESSION['users']["password"]."<br/>"?><p>
+        <p>Address : <?php echo $_SESSION['users']["address"]."<br/>"?><p>
+        <p>Role : <?php echo $_SESSION['users']["role"]."<br/>"?><p>
+        <p>Status : <?php if($_SESSION['users']["status"] == 1){
+                    echo "Active";
+                }else{
+                    echo "Inactive";
+                }?>
+        <p>
+    </center>
+    
+</body>
+</html>
---
+++ 4-March-2025/login_session_2d/teacherDashboard.php
@@ -0,0 +1,67 @@
+<?php
+    session_start();
+   
+    if(isset($_SESSION['users'])){
+        if($_SESSION['users']['role'] == "admin"){
+            header("location: adminDashboard.php");
+        }elseif($_SESSION['users']['role'] == "student"){
+            header("location: studentDashboard.php");
+        }
+    }else{
+        header("location: login_form.php?msg=Please Login First...");
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Teacher Dashboard</title>
+    <style>
+        body{
+            background-color: green;
+        }
+        h1{
+            color: blue;
+            background-color: lightgray;
+        }
+        h2{
+            color:white;
+        }
+        a{
+            text-decoration: none;
+            color: black;
+            background-color: deepskyblue;
+            border: 1px solid black;
+        }
+        a:hover{
+            color:red;
+        }
+        p{
+            color:white;
+        }
+    </style>
+</head>
+<body>
+    <center>
+
+        <h1>Teacher Dashboard</h1>
+        <hr>
+        <h2>Welcome... <?php echo $_SESSION['users']["first_name"]." ".$_SESSION['users']["last_name"]; ?></h2>
+        <a href="logout.php">Logout</a>
+        <p>First Name : <?php echo $_SESSION['users']["first_name"]."<br/>"?><p>
+        <p>Last Name : <?php echo $_SESSION['users']["last_name"]."<br/>"?><p>
+        <p>Email : <?php echo $_SESSION['users']["email"]."<br/>"?><p>
+        <p>Password : <?php echo $_SESSION['users']["password"]."<br/>"?><p>
+        <p>Address : <?php echo $_SESSION['users']["address"]."<br/>"?><p>
+        <p>Role : <?php echo $_SESSION['users']["role"]."<br/>"?><p>
+        <p>Status : <?php if($_SESSION['users']["status"] == 1){
+                    echo "Active";
+                }else{
+                    echo "Inactive";
+                }?><p>
+    </center>
+    
+</body>
+</html>
