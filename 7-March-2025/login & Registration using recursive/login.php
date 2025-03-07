a8c51375b00c3260358a5121cf67a63cd4c01109
Ahsan Ali <93969884+ahsanaliSWE@users.noreply.github.com>
2025-03-07 22:31:26 +0500

    Delete 7-March-2025/login & Registration using recursive directory

--- 7-March-2025/login & Registration using recursive/login.php
+++
@@ -1,8 +0,0 @@
-<?php
-    include 'functions.php';
-    /* $method = "POST";
-    $action = "login.php";
-    login_form($method,$action); */
-
-    login_form();
-?>
--- 7-March-2025/login & Registration using recursive/registration_form.php
+++
@@ -1,7 +0,0 @@
-<?php
-    include 'functions.php';
-    /* $method = "POST";
-    $action = "registration_form.php";
-    registration_form($method,$action); */
-    registration_form();
-?>
